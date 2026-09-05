8abb51b2df280295250e44cd56a042410f5ffc1c
asterixhmk <yanishamek14@gmail.com>
2026-09-05 16:28:51 -0400

    Start API before in-process worker

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,19 @@ import { buildApp } from "./app.js";
 
 const app = buildApp();
 
-if (env.START_WORKER_IN_PROCESS) {
-  await import("./queue/worker.js");
-  app.log.info("Cluster POS worker started in API process");
-}
-
 app
   .listen({ port: env.PORT, host: env.HOST })
   .then((address) => {
     app.log.info(`cluster-pos-hub listening on ${address}`);
+    if (env.START_WORKER_IN_PROCESS) {
+      void import("./queue/worker.js")
+        .then(() => {
+          app.log.info("Cluster POS worker started in API process");
+        })
+        .catch((err) => {
+          app.log.error({ err }, "Cluster POS worker failed to start");
+        });
+    }
   })
   .catch((err) => {
     app.log.error(err);
